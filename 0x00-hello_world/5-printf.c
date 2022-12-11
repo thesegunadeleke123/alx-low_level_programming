f1742ec24643690e7d656c4deba705641bc9baec
Segun Adeleke <thesegunadeleke123@gmail.com>
2022-12-11 16:46:38 +0000

    Add task using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - function that uses printf to print a set of chars
+ *
+ * return: 0 if successful.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
