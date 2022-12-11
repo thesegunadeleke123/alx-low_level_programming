d71c22bb54eec53ba16aea796152ae8afc3bee28
Segun Adeleke <thesegunadeleke123@gmail.com>
2022-12-11 16:48:29 +0000

    Add task using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
 /**
  * main - function that uses printf to print a set of chars
  *
- * return: 0 if successful.
+ * Return: 0 if successful.
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
