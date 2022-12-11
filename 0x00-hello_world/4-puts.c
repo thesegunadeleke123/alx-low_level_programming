0c9c03e0eaf2a6a48367663e65a8863cf3fba579
Segun Adeleke <thesegunadeleke123@gmail.com>
2022-12-11 16:39:09 +0000

    Add Puts task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
 
+/**
+ * main - Prints a string of char
+ *
+ * Description: using the function puts to
+ * print the strings as given in the task.
+ * Return: 0
+ */
+
 int main()
 {
 	puts("\"Programming is like building a multilingual puzzle");
