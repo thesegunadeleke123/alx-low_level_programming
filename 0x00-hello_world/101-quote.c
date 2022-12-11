c655009aa91ae80805cb1eb0b5aec36be543215c
Segun Adeleke <thesegunadeleke123@gmail.com>
2022-12-11 17:18:44 +0000

    Add 101 task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - prints characters as specified.
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	write(2,"and the peice of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
