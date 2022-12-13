cd3133baa100d972e9b000ea29ed7fb7a1d4bf75
Segun Adeleke <thesegunadeleke123@gmail.com>
2022-12-13 22:07:09 +0000

    Add task 2

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - checks character, returns 1 if lowercase
+ * or 0 if other case
+ *
+ * Return: Based on character.
+ */
+
+int _islower()
+{
+	char c;
+
+	if (c => 97 && c <= 122)
+		return (1);
+	else 
+		return (0);
+}
