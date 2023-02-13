6b05b5eeb49d1a08102ae65d459ea916ddc9e65a
Segun Adeleke <thesegunadeleke123@gmail.com>
2023-02-13 13:17:15 +0000

    Add Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,13 +7,17 @@
 
 int print_sign(int n)
 {
-	int n;
-
 	if(n > 0)
 	{
 		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
-		return (1);
+		return 1;
 	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return 0;
+	}
+	else
+		_putchar('-');
+		return -1;
 }
