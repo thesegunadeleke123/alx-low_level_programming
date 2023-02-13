2d2fc610110b91fcd5bcfca30c1d0bc29f98cd73
Segun Adeleke <thesegunadeleke123@gmail.com>
2023-02-13 13:58:38 +0000

    Add Task 6'

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - function to return absolute value
+ * of an integer.
+ * @r: variable whose absolute value is to be
+ * returned.
+ * 
+ * return: 0 if successful
+*/
+
+int _abs(int r)
+{
+    if (r >= 0)
+    {
+        return r;
+    }
+    else if (r < 0)
+    {
+        return r * (-1);
+    }
+}
