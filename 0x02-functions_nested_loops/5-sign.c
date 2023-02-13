5eb727c3993d2d449c4963ecbf165d5535b3e41a
Segun Adeleke <thesegunadeleke123@gmail.com>
2023-02-13 13:21:43 +0000

    Add documentation to Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,25 +1,27 @@
 #include "main.h"
 
 /**
+ * print_sign - print sign attached to a number
+ * and return a particular value.
  *
- *
+ * Return: 0 if positive.
  */
 
 int print_sign(int n)
 {
-	if(n > 0)
+	if (n > 0)
 	{
 		_putchar('+');
-		return 1;
+		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		return 0;
+		return (0);
 	}
 	else
 	{
 		_putchar('-');
-		return -1;
+		return (-1);
 	}
 }
